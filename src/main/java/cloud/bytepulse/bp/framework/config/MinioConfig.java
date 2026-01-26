618c0d7d38c17358fd17bf1731f2893457e87cb5
jiejiebiezheyang <1964234252@qq.com>
2026-01-26 18:08:18 +0800

    feat: 为MinioClient添加自定义OkHttpClient配置

--- src/main/java/cloud/bytepulse/bp/framework/config/MinioConfig.java
+++ src/main/java/cloud/bytepulse/bp/framework/config/MinioConfig.java
@@ -3,8 +3,12 @@ package cloud.bytepulse.bp.framework.config;
 import cloud.bytepulse.bp.common.util.minio.properties.IMinioProperties;
 import io.minio.MinioClient;
 import lombok.RequiredArgsConstructor;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author jiejiebiezheyang
@@ -18,8 +22,17 @@ public class MinioConfig {
 
     @Bean
     public MinioClient minioClient() {
+        // 自定义 OkHttpClient
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectionPool(new ConnectionPool(20, 5, TimeUnit.MINUTES)) // 最大20连接，空闲存活5分钟
+                .connectTimeout(10, TimeUnit.SECONDS) // 连接超时
+                .readTimeout(60, TimeUnit.SECONDS)    // 读超时
+                .writeTimeout(60, TimeUnit.SECONDS)   // 写超时
+                .build();
+
         return MinioClient.builder().endpoint(
                         iMinioProperties.getEndpoint())
+                .httpClient(okHttpClient)
                 .credentials(iMinioProperties.getAccessKey(),
                         iMinioProperties.getSecretKey()).build();
     }
