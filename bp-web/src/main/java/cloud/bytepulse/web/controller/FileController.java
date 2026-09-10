ea45159ab6c4cab497166c847398e9670232736e
jiejiebiezheyang <1964234252@qq.com>
2026-09-10 17:47:04 +0800

    feat(file): 添加上传测试接口

--- bp-web/src/main/java/cloud/bytepulse/web/controller/FileController.java
+++ bp-web/src/main/java/cloud/bytepulse/web/controller/FileController.java
@@ -3,15 +3,14 @@ package cloud.bytepulse.web.controller;
 import cloud.bytepulse.common.core.annotation.Anonymous;
 import cloud.bytepulse.common.core.annotation.BpLogging;
 import cloud.bytepulse.common.core.enums.OperateEnum;
+import cloud.bytepulse.common.core.model.ApiResponse;
 import cloud.bytepulse.service.file.FileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author jiejiebiezheyang
@@ -38,5 +37,17 @@ public class FileController {
     @BpLogging(value = OperateEnum.QUERY, desc = "文件访问, 公开")
     public ResponseEntity<Void> access(@PathVariable Long fileId) throws Exception {
         return fileService.access(fileId);
+    }
+
+    @PostMapping("/access/upload")
+    @Operation(summary = "文件上传测试")
+    @Anonymous
+    @BpLogging(value = OperateEnum.UPLOAD, desc = "文件上传测试")
+    public ApiResponse<Void> upload(@RequestPart MultipartFile[] files,
+                                    @RequestPart String bizId) throws Exception {
+        for (MultipartFile file : files) {
+            fileService.uploadFile("test", bizId, file, true, false);
+        }
+        return ApiResponse.success();
     }
 }
